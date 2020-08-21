65820c422967004123f112330a3fc1dea85bd18f
Andres Alban <bethesdas101@gmail.com>
2020-08-21 16:51:00 -0500

    Adding date and color picker to calendar app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ModalEventComponent } from './modal-event/modal-event.component';
+import { AngularMyDatePickerModule } from 'angular-mydatepicker';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { RemainderService } from './services/remainders.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    ModalEventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularMyDatePickerModule,
+    HttpClientModule,
+    ColorPickerModule
   ],
-  providers: [],
+  providers: [RemainderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
