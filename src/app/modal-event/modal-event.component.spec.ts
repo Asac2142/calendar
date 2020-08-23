6e266961ed4b8ed01dbc8669211a566ac9a7b49b
Andres Alban <bethesdas101@gmail.com>
2020-08-23 09:40:45 -0500

    Adding unit test for modal-event component, creating a remainder

--- src/app/modal-event/modal-event.component.spec.ts
+++ src/app/modal-event/modal-event.component.spec.ts
@@ -1,25 +1,56 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ModalEventComponent } from './modal-event.component';
+import { Remainder } from '../model/remainder';
+import { ModalEventComponent } from "./modal-event.component";
 
 describe('ModalEventComponent', () => {
-  let component: ModalEventComponent;
-  let fixture: ComponentFixture<ModalEventComponent>;
+  const fakeRemainderService: any = jasmine.createSpyObj('remainderService', ['setRemainder', 'filterByScheduledDay']);
+  const fakeWeatherService: any = jasmine.createSpyObj('weatherService', ['getForecastWeatherByCity']);
+  const fakeCityService: any = jasmine.createSpyObj('citiyService', ['getCities']);
+  
+  fakeRemainderService.setRemainder();
+  fakeRemainderService.remainders = []
+  fakeRemainderService.filterByScheduledDay();
+  const remainderObject = new Remainder(      
+    {
+      dateRange: null, 
+      isRange: false, 
+      singleDate: {
+        date: {
+          year: 2200,
+          month: 8,
+          day: 7
+        },
+        formatted: '07.08.2200'
+      }
+    }, '7', '_7_1_#ccc', 'Lunch at office', 'Ecuador', '12:31', '#ccc', 'Rain');
+  
+  it('should allow to save a remainder', () => {    
+    const modalRemainder = new ModalEventComponent(fakeCityService, fakeRemainderService, fakeWeatherService);
+    
+    modalRemainder.remainderDate = { 
+      dateRange: null,  
+      singleDate: {
+        date: {
+          year: 2200, 
+          month: 8, 
+          day: 7,
+          formatted: '07.08.2200'
+        }
+      }, 
+      isRange: false
+    };
+    modalRemainder.modalRemainder.day = '7';
+    modalRemainder.remainderId = '_7_1_#ccc';
+    modalRemainder.remainder = 'Lunch at office';
+    modalRemainder.selectedCity = 'Ecuador';
+    modalRemainder.time = '12:31';
+    modalRemainder.color = '#ccc';
+    modalRemainder.weather = 'Rain';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ModalEventComponent ]
-    })
-    .compileComponents();
-  }));
+    
+    fakeRemainderService.remainders.push(remainderObject);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ModalEventComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    modalRemainder.onSaveRemainder();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(fakeRemainderService.setRemainder()).toHaveBeenCalled();
   });
 });
